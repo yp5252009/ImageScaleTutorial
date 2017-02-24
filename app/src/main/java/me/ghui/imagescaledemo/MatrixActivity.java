01dc9fd918b203af01beca25824eb5ef3d5f6938
ghui <ghuiii.zhang@gmail.com>
2017-02-25 01:03:27 +0800

    update

--- app/src/main/java/me/ghui/imagescaledemo/MatrixActivity.java
+++ app/src/main/java/me/ghui/imagescaledemo/MatrixActivity.java
@@ -1,7 +1,7 @@
 package me.ghui.imagescaledemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
